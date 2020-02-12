993ed49bf4b3a56d35479cd94aa75d0d8cdfe52b
aumdove <aumdove13@gmail.com>
2020-02-12 14:58:37 -0600

    Added heroku config

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const compression = require('compression');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
--- server.js
+++ server.js
@@ -38,3 +38,9 @@ process.on('unhandledRejection', err => {
   });
 });
 
+process.on('SIGTERM', () => {
+  console.log('👋🏼 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
